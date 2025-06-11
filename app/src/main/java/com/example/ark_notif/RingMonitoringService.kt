896a02d444ff8a2f2941f4959b2f9731d21cca6b
Kurt Bryant <BCArpilleda@gmail.com>
2025-06-11 15:27:06 +0800

    adsa

--- app/src/main/java/com/example/ark_notif/RingMonitoringService.kt
+++ app/src/main/java/com/example/ark_notif/RingMonitoringService.kt
@@ -57,7 +57,7 @@ class RingMonitoringService : Service(), SharedPreferences.OnSharedPreferenceCha
     companion object {
         private const val CHANNEL_ID = "RingMonitoringChannel"
         private const val NOTIFICATION_ID = 1234
-        private const val MONITORING_INTERVAL = 8_000L
+        private const val MONITORING_INTERVAL = 6_000L
         private const val RESTART_INTERVAL = 300_000L
         const val ACTION_START_MONITORING = "START_MONITORING"
         const val ACTION_STOP_MONITORING = "STOP_MONITORING"

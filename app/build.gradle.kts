b95fccb8f82d720c525d8feb12823042027a7bb8
Kurt Bryant <BCArpilleda@gmail.com>
2025-07-15 15:36:12 +0800

    adsa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.ark_notif"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.9"
+        versionCode = 21
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
